117063b957bd34ea28f5c0ed3a852168a5d9451c
nickyamc <namc.devel@gmail.com>
2023-07-31 10:52:47 -0500

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, QRCode } from "antd";
+import { Button, Col, Form, Input, QRCode, Row } from "antd";
 import React, { useState } from "react";
 import ImgLogo from "./assets/logo_unamad.png";
 
@@ -21,27 +21,38 @@ const App: React.FC = () => {
   const [text, setText] = useState<string>("");
   const [nameFile, setNameFail] = useState<string>("");
   return (
-    <div id="myqrcode">
-      <Input
-        placeholder="Nombre del archivo"
-        onChange={(e) => setNameFail(e.target.value)}
-      />
-      <Input
-        placeholder="URL a QRCode"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <QRCode
-        type="canvas"
-        value={text}
-        style={{ marginBottom: 16 }}
-        icon={ImgLogo}
-        errorLevel="H"
-        size={300}
-        iconSize={300 / 4}
-      />
-      <Button type="primary" onClick={() => downloadQRCode(nameFile)}>
-        Download
-      </Button>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Row>
+        <Col span={12} style={{display: "flex", flexDirection: "column", gap: 25}}>
+          <h1>Generador de QRCode</h1><Input
+            placeholder="Nombre del archivo"
+            onChange={(e) => setNameFail(e.target.value)}
+          />
+          <Input
+            placeholder="URL a QRCode"
+            onChange={(e) => setText(e.target.value)}
+          /><Button type="primary" onClick={() => downloadQRCode(nameFile)}>
+            Download
+          </Button>
+        </Col>
+        <Col span={12}>
+          <div style={{ width: 300 }} id="myqrcode">
+            <QRCode
+              type="canvas"
+              value={text}
+              style={{ marginBottom: 16 }}
+              icon={ImgLogo}
+              errorLevel="H"
+              size={300}
+              iconSize={300 / 4}
+            />
+          </div>
+
+        </Col>
+      </Row>
+
+
+
     </div>
   );
 };
